e47fe20765f996483f5118939bb102a488e3c6ca
Maksim <honestliar91@gmail.com>
2021-06-16 13:38:47 +0300

    praktika2

--- js/script.js
+++ js/script.js
@@ -8,13 +8,37 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+if (personalMovieDB.count < 10) {
+    alert('few movies');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('normal');
+} else {
+    alert('many');
+}
 
-const a = prompt("какой фильм?", ""),
-    b = prompt('какая оценка', ''),
-    c = prompt("какой фильм?", ""),
-    d = prompt('какая оценка', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+for (let i = 1; i <= personalMovieDB.count; i++) {
+    let x = 0,
+        a = prompt("какой фильм?", "");
+    while (x < 1) {
+        if (!a) {
+            a = prompt("какой фильм?", "");
+        } else if (a.length == 0 || a.length > 50) {
+            a = prompt("какой фильм?", "");
+        } else {
+            x++;
+        }
+    }
+    x = 0;
+    let b = prompt('какая оценка', '');
+    while (x < 1) {
+        if (!b) {
+            b = prompt('какая оценка', '');
+        } else if (b.length == 0 || b.length > 50) {
+            b = prompt('какая оценка', '');
+        } else {
+            x++;
+        }
+    }
+    personalMovieDB.movies[a] = b;
+}
 console.log(personalMovieDB);
