f70416ae2ce2020c6dd28401c4a48d3643673786
Maksim <honestliar91@gmail.com>
2021-06-16 11:49:00 +0300

    praktika1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 "use strict";
-const messedg = (confirm("name is maks", ""));
-console.log(messedg);
+let numbersOfFilms = +prompt("how match films youu watch?", "");
+console.log(numbersOfFilms);
+const personalMovieDB = {
+    count: numbersOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("какой фильм?", ""),
+    b = prompt('какая оценка', ''),
+    c = prompt("какой фильм?", ""),
+    d = prompt('какая оценка', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
