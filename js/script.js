1857f5e1ca4da15839470e8c867bf082bf9e50f7
Maksim <honestliar91@gmail.com>
2021-06-18 16:43:43 +0300

    praktika4

--- js/script.js
+++ js/script.js
@@ -1,70 +1,74 @@
 "use strict";
 let numbersOfFilms;
-
-function start() {
-    while (numbersOfFilms == '' || numbersOfFilms == null || isNaN(numbersOfFilms)) {
-        numbersOfFilms = +prompt("how match films do you watch?", "");
-    }
-}
-start();
-
 const personalMovieDB = {
-    count: numbersOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start() {
+        while (numbersOfFilms == '' || numbersOfFilms == null || isNaN(numbersOfFilms)) {
+            numbersOfFilms = +prompt("how match films do you watch?", "");
+            personalMovieDB.count = numbersOfFilms;
+            if (personalMovieDB.count < 10) {
+                alert('few movies');
+            } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+                alert('normal');
+            } else {
+                alert('many');
+            }
+        }
+    },
+    howMivies() {
+        for (let i = 1; i <= personalMovieDB.count; i++) {
+           let a = prompt("какой фильм?", "");
+            while (true) {
+                if (!a) {
+                    a = prompt("какой фильм?", "");
+                } else if (a.length == 0 || a.length > 50) {
+                    a = prompt("какой фильм?", "");
+                } else {
+                    break;
+                }
+            }
+            let b = prompt('какая оценка', '');
+            while (true) {
+                if (!b) {
+                    b = prompt('какая оценка', '');
+                } else if (b.length == 0 || b.length > 50) {
+                    b = prompt('какая оценка', '');
+                } else {
+                    break;
+                }
+            }
+            personalMovieDB.movies[a] = b;
+        }
+    },
+    writeYourGenres() {
+        for (let i = 1; i <= 3; i++) {
+            let a = prompt('how is your ' + i + ' favarite genres?', '');
+            while (true) {
+                if (a == '' || a == null) {
+                    a = prompt('how is your ' + i + ' favarite genres?', '');
+                } else {
+                    break;
+                }
+            }
+            personalMovieDB.genres[i - 1] = a;
+        }
+        personalMovieDB.genres.forEach(function(gen, index) {
+            console.log(`Любимфй жанр №${index + 1} - это ${gen}.`);
+        });
+    },
+    showMyDB() {
+        if (!personalMovieDB.privat) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyBD(){
+        personalMovieDB.privat = !personalMovieDB.privat;
+    },
 };
 
-
-if (personalMovieDB.count < 10) {
-    alert('few movies');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert('normal');
-} else {
-    alert('many');
-}
-
-function howMivies() {
-    for (let i = 1; i <= personalMovieDB.count; i++) {
-        let x = 0,
-            a = prompt("какой фильм?", "");
-        while (x < 1) {
-            if (!a) {
-                a = prompt("какой фильм?", "");
-            } else if (a.length == 0 || a.length > 50) {
-                a = prompt("какой фильм?", "");
-            } else {
-                x++;
-            }
-        }
-        x = 0;
-        let b = prompt('какая оценка', '');
-        while (x < 1) {
-            if (!b) {
-                b = prompt('какая оценка', '');
-            } else if (b.length == 0 || b.length > 50) {
-                b = prompt('какая оценка', '');
-            } else {
-                x++;
-            }
-        }
-        personalMovieDB.movies[a] = b;
-    }
-}
-
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt('how is your ' + i + ' favarite genres?', '');
-    }
-}
-
-writeYourGenres();
-
-function showMyDB() {
-    if (!personalMovieDB.privat) {
-        console.log(personalMovieDB);
-    }
-}
-showMyDB();
+personalMovieDB.writeYourGenres();
+personalMovieDB.showMyDB();
